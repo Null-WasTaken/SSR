f805c2d76e645e11407351c15d014b08f5f607ee
Amir Hossein Fallah <amirfallah1385@gmail.com>
2023-07-29 20:19:06 +0330

    Hello

--- src/Components/Ana.jsx
+++ src/Components/Ana.jsx
@@ -4,7 +4,9 @@ export function Ana() {
     return(
         <div className={"w-full bg-white py-16 px-4"}>
             <div className={"max-w-[1240px] mx-auto grid md:grid-cols-2"}>
-
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo facilis praesentium ut voluptates. Blanditiis commodi eaque hic, illo iusto necessitatibus nihil omnis possimus provident quidem quo repellendus, sequi tempora voluptate.
+                </p>
             </div>
         </div>
     )
